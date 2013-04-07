3c5a7233aa1c26a630df83969768d9088c8ada83
Dominique Jullier <domjullier@gmail.com>
2013-04-07 21:44:00 +0200

    update of file structure

--- DPD_main.py
+++ DPD_main.py
@@ -5,12 +5,17 @@ Created on Mar 15, 2013
 
 Non object oriented version
 '''
+
+import copy
+import csv
 from Vector import Vector
 from Particle import Particle
 from random import randrange
 
 if __name__ == '__main__':
     pass
+
+
 
 '''
 Generates initial condition with based on the given particle. 
@@ -25,6 +30,16 @@ def genInitCond(numOfParticles, spaceSize, mass, radius):
         
     return particles
 
+def genInitCond2(ptype, numOfParticles, spaceSize, mass, radius):
+    
+    particles=[]
+       
+    for _ in range(numOfParticles):
+        '''currentParticle = [type, mass, radius, randrange(0, spaceSize), randrange(0, spaceSize), randrange(0, spaceSize), 0, 0, 0, 0, 0, 0]'''
+        particles.append([ptype, mass, radius, randrange(0, spaceSize), randrange(0, spaceSize), randrange(0, spaceSize), 0, 0, 0, 0, 0, 0])
+        
+    return particles
+
 def gravity(particle, step):
     if(particle.position.z<=0):
         particle.position.z=0
@@ -36,21 +51,43 @@ def gravity(particle, step):
     else:
         particle.acceleration+=Vector(0,0,(-9.82*step))
         return particle
+    
+def gravity2(particle, step):
+    if(particle[5]<=0):
+        particle[5]=0 #position z
+        particle[11]=0 #acceleration z
+        particle[8]=0 #velocity z
+
+        return particle
+    
+    else:
+        particle[11]+=(-9.82*step)
+        return particle
         
 def calculateNewPositions(particles, step):
     for p in particles:
 
-        currentVelocity=Vector(p.velocity.x, p.velocity.y, p.velocity.z)
-        p.velocity+=p.acceleration
-
-        p.position+=((currentVelocity+p.velocity)/2)*step
+        currentVelocity=(p[6], p[7], p[8])
         
-        if p.position.z<=0:
-            p.position.z=0
-            p.velocity.z=0
+        #calculate current velocity + acceleration
+        p[6]+=p[9]
+        p[7]+=p[10]
+        p[8]+=p[11]
+        
+        #calculate new position
+        p[3]+=((currentVelocity[0]+p[6])/2)*step
+        p[4]+=((currentVelocity[1]+p[7])/2)*step
+        p[5]+=((currentVelocity[2]+p[8])/2)*step
+
+        
+        if p[5]<=0: #pos z
+            p[5]=0
+            p[8]=0 #velo z
             
 
-        p.acceleration=Vector(0,0,0)
+        p[9]=0
+        p[10]=0
+        p[11]=0
 
         
     return particles
@@ -68,6 +105,19 @@ def nextStep(particles, step):
     
     return particles
 
+def nextStep2(state, step):
+    
+    '''apply gravity'''
+    for p in state:
+        p=gravity2(p, step)
+    
+    '''apply more forces...'''
+    
+    '''Calculate new positions'''
+    particles = calculateNewPositions(state, step)
+    
+    return particles
+
 def getListsForVisualization(particleWorld):
     x = []
     y = []
@@ -76,23 +126,70 @@ def getListsForVisualization(particleWorld):
     newList = [[]]
     
     newList.append(x, y, z)
+    
+'''#Full structure
+|Full structure        |number of particles    |number of states    |time step between states    |size of world
+|type of particle    |mass    |radius        |position x|y|z    |velocity x|y|z    |acceleration x|y|z
+|type of particle    |mass    |radius        |position x|y|z    |velocity x|y|z    |acceleration x|y|z
+...
+'''
+    
 
-particle_world = []
-start = genInitCond(numOfParticles=10, spaceSize=1000, mass=1, radius=1)
+state = genInitCond2(ptype=1, numOfParticles=20, spaceSize=1000, mass=1, radius=1)
+state.extend(genInitCond2(ptype=2, numOfParticles=20, spaceSize=1000, mass=1, radius=1))
 
-particle_world.append(start)
-
-print "Beginning"
-for p in start:
-    print p
+'''add initial state to history'''
+world_history = []
+world_history.extend(copy.deepcopy(state))
 
 
 
-for i in range(10):
-    start=nextStep(start, 0.1)
-    particle_world.append(start)
-    print "Step %s" % (i)
-    for p in start:
-        print p
 
+world_history.extend(copy.deepcopy(state))
+
+#print world_history 
+     
+
+for i in range(1, 1500):
+    state=nextStep2(state, 0.01)
+    world_history.extend(copy.deepcopy(state))
+    #print "Step %s" % (i)
+    #for p in state:
+    #    print p
+
+#print world_history 
+
+#|Full structure        |number of particles    |number of states    |time step between states    |size of world
+
+f = open('workfile', 'wb')
+
+#write fileheader
+numberOfStates = 0
+numberOfParticles = 40
+
+for _ in world_history:
+    numberOfStates+=1
+    
+numberOfStates/=numberOfParticles
+
+header = [1,numberOfParticles, numberOfStates, 0.01, 1000]
+
+wr = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+
+wr.writerow(header)
+wr.writerow("")
+wr.writerow([1, 1, 1])
+wr.writerow([2, 1, 1])
+
+wr.writerow("")
+cnt = 0
+for line in world_history:
+    if cnt == numberOfParticles:
+        wr.writerow("")
+        cnt=0
+        
+    cnt=cnt+1
+    wr.writerow(line[:1] + line[3:9])
+    
+#wr.writerow(line[:1] + line[3:6])
 print "end"
