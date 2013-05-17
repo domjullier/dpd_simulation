b77fccd277ba1e6b9d7e6aaa822acab1a237625a
skaras <skaras@student.agh.edu.pl>
2013-05-17 18:24:47 +0200

    Fix previous incorrect commit: vizer added new arguments.

--- viz/vizer.py
+++ viz/vizer.py
@@ -3,23 +3,31 @@
 
 # Argumenty: (??)
 # repeat - powtarzanie po dobiegnieciu do konca
-# first, last - kroki od-do.
-# logowanie jakiejś informacji co x kroków?
 
-import argparse, os, logging
-from wx import Timer
-from mayavi import mlab
 
 # Program arguments.
+import argparse, sys
+
 aparser = argparse.ArgumentParser()
-aparser.add_argument('result_dir', help='directory containing visualisation\
-        data')
+aparser.add_argument('result_dir',\
+        help='directory containing visualisation data')
+aparser.add_argument('--first', default=0, type=int,\
+        help='first step of an animation (default: 0)')
+aparser.add_argument('--last', default=sys.maxint, type=int,\
+        help='last step of an animation (default: last)')
+aparser.add_argument('--info_every', type=int, default=sys.maxint,\
+        help='prints information every X steps.')
+        
 
 args = aparser.parse_args()
 
+import os, logging
+from wx import Timer
+from mayavi import mlab
+
 # Logging configuration.
 log = logging.getLogger('vizer')
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 
 han = logging.StreamHandler()
 han.setLevel(logging.DEBUG)
@@ -45,17 +53,45 @@ class WorldDescription(object):
 class ResultProcessor(object):
     resDir = ''
     wDesc = WorldDescription()
+    curStep = 0
+    endStep = 0
+    headFile = ''
+    fileList = []
 
-    def __init__(self, resDir):
+    def __init__(self, resDir, stepA, stepZ, infoEvery):
+        log.debug('ResultProcessor starting.')
         self.resDir = resDir
+        self.fileList = [os.path.join(resDir, f) for f in os.listdir(resDir)]
+        self.fileList.sort()
+        self.headFile = self.fileList.pop(0)
+
+        self.curStep = stepA
+        self.endStep = (len(self.fileList)-1, stepZ)[stepZ < len(self.fileList)]
+
+        log.info('Results directory: %s, steps from %d to %d.',\
+                self.resDir, self.curStep, self.endStep)
+
+        self.infoEvery = infoEvery
+        self.infoOffst = self.curStep % self.infoEvery
+
+        log.debug('Animating files from %s, starting with %s up to %s.',\
+                self.resDir, self.fileList[self.curStep],\
+                self.fileList[self.endStep])
 
     def processHeader(self):
-        with file(os.path.join(self.resDir, 'result'), 'r') as infile:
+        log.info('Reading header from file %s.', self.headFile)
+        with file(self.headFile, 'r') as infile:
             lines = [l.strip() for l in infile.xreadlines()]
-            #lines = [l.strip() for l in infile.xreadlines() if l and l.strip()[0] != '#']
+            # TODO komentarze?
+            #lines = [line for line in [l.strip() for l in infile.xreadlines()]\
+            #        if line and not line.startswith('#')]
 
             # 1st line - world description
             desc = lines[0].split(',')
+
+            log.debug('Headers 1st line: %s', lines[0])
+            assert len(desc) > 4, 'Headers 1st line does not contain necessary\
+                    fields!'
 
             self.wDesc.particles_n      = int(desc[1])
             self.wDesc.states_n         = int(desc[2])
@@ -67,7 +103,10 @@ class ResultProcessor(object):
             # 2nd line empty, others - particle types
             for i in xrange(2,len(lines)):
                 typel = lines[i].split(',')
-                log.debug('Typeline[%d]: %s', i, typel[0])
+
+                log.debug('Particle type: %s', lines[i])
+                assert len(typel) > 2, 'Particle type description does not\
+                        contain necessary fields!'
 
                 parttype = ParticleType()
                 parttype.n = int(typel[0])
@@ -76,9 +115,20 @@ class ResultProcessor(object):
 
                 self.wDesc.ptypes.append(parttype)
 
-    def processStep(self, n):
+    def processStep(self):
+        if self.curStep > self.endStep:
+            raise StopIteration
+
         x, y, z, s = [], [], [], []
-        with file(os.path.join(self.resDir, 'result_step_' + str(n)), 'r') as infile:
+
+        stepFile = self.fileList[self.curStep]
+
+        if self.curStep % self.infoEvery == self.infoOffst:
+            log.info('Animated step %d (file: %s).', self.curStep, stepFile)
+
+        self.curStep += 1
+
+        with file(stepFile, 'r') as infile:
             for line in infile.xreadlines():
                 spl = line.strip().split(',')
                 x.append(float(spl[1]))
@@ -86,7 +136,10 @@ class ResultProcessor(object):
                 z.append(float(spl[3]))
                 s.append(float(spl[0]))
 
-            assert len(x) == self.wDesc.particles_n # TODO dodać kolejne??
+            assert len(x) == self.wDesc.particles_n
+            assert len(y) == self.wDesc.particles_n
+            assert len(z) == self.wDesc.particles_n
+            assert len(s) == self.wDesc.particles_n
             return (x,y,z,s)
 
 
@@ -96,32 +149,28 @@ class Animator(Timer):
         Timer.__init__(self)
         self.ms = mlab_source
         self.fproc = fileproc
-        self.step_n = 1
-        self.step_delay = 1; #ms
+        self.stepDelay = 1; #ms
 
     def Animate(self):
         log.debug('Animator starting.')
-        self.Start(self.step_delay, True)
+        self.Start(self.stepDelay, True)
 
     def Notify(self):
         try:
-            s = self.fproc.processStep(self.step_n)
-            self.step_n += 1
-            self.ms.set(x = s[0], y = s[1], z = s[2], scalar=s[3])
-            self.Start(self.step_delay, True)
-        except EOFError:
-            log.warn('End of file.') # TODO na pewno?
-        except IOError:
-            log.info('Reached end of animation.')
+            x,y,z,s = self.fproc.processStep()
+            self.ms.set(x = x, y = y, z = z, scalar=s)
+            self.Start(self.stepDelay, True)
+        except StopIteration:
+            log.info('Reached the end of animation.')
 
 def main():
     log.info('Program starting.')
-    log.info('Results directory: %s', args.result_dir)
-    log.info('Steps: %d to %d.', 0, len(os.listdir(args.result_dir))-2)
 
-    rp = ResultProcessor(args.result_dir)
+    rp = ResultProcessor(args.result_dir, args.first, args.last,\
+            args.info_every)
+
     rp.processHeader()
-    s = rp.processStep(0)
+    s = rp.processStep()
 
     s = mlab.points3d(s[0], s[1], s[2], s[3], colormap="autumn",
             scale_factor=15)
