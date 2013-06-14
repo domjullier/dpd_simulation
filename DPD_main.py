7d77ee5d93270f3525514223997cb1a54d994d53
skaras <skaras@student.agh.edu.pl>
2013-06-14 20:17:52 +0200

    Code beautification, improvements described in poprawki.txt

--- DPD_main.py
+++ DPD_main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2
+# -*- coding: utf8 -*-
 '''
 Created on Mar 15, 2013
 
@@ -11,66 +13,11 @@ import ConfigParser
 from random import randrange
 
 #------------------------------------------------------------------------------.
-def ConfigSectionMap(section):
-    dict1 = {}
-    options = Config.options(section)
-    for option in options:
-        try:
-            dict1[option] = Config.get(section, option)
-            if dict1[option] == -1:
-                print ("skip: %s" % option)
-        except:
-            print("exception on %s!" % option)
-            dict1[option] = None
-    return dict1
+# List contatining particles. Main data structure.
+Universe = []
 
 #------------------------------------------------------------------------------.
-def write_Header(simulatedSteps, totalNumberOfParticles, timePerStep, spacesize):
-    f = open(args.outfile, 'wb')
-    
-    header = [1,totalNumberOfParticles, simulatedSteps, timePerStep, spacesize]
-    
-    wr = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-    
-    wr.writerow(header)
-    wr.writerow("")
-    
-    i=1
-    for (n, m) in zip(numberOfParticles,mass):
-        wr.writerow([i, n, m])
-        i+=1
-
-#------------------------------------------------------------------------------.
-def save_state(state, stepnr):
-    #filename = args.outfile + "_step_" + str(stepnr).zfill(6)
-    filename = args.outfile + "_step_" + str(stepnr).zfill(6)
-    f = open(filename, 'wb')
-    wr = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-    
-    for line in state:
-        wr.writerow(line[:1] + line[3:9])
-    
-    f.close()
-    pass
-
-#------------------------------------------------------------------------------.
-def conservativeForce(Sc, R, Ex, Ey, Ez):
-    # temporary
-    T = Sc * (1.0 - R)
-    return (T*Ex, T*Ey, T*Ez)
-
-def dissipativeForce(Sd, R, Vx, Vy, Vz, Ex, Ey, Ez):
-    # temporary
-    T = Sd*(1.0-R)**2 * (Vx*Ex + Vy*Ey + Vz*Ez)
-    return (T*Ex, T*Ey, T*Ez)
-
-def randomForce(Sr,Sd,kB,temp,delta_t, R, Ex, Ey, Ez):
-    #temporary
-    T = (2*Sd*kB*(temp/delta_t))**0.5 * (1.0 - R) * Sr
-    return (T*Ex, T*Ey, T*Ez)
-
-#------------------------------------------------------------------------------.
-
+# Particle is a tuple consisting of (numbers are indexes):
 Type = 0
 Mass = 1
 Rads = 2
@@ -88,45 +35,129 @@ Ay = 10
 Az = 11
 
 #------------------------------------------------------------------------------.
+class SimParams:
+    SpaceSize       = 0
+    StepsN          = 0
+    Temperature     = 0
+    StepTime        = 0
+
+    ConsForceFactor = 0
+    DissForceFactor = 0
+    RandForceFactor = 0
+
+    GravityFactor   = 0
+    CutoffRadius    = 0
+
+#------------------------------------------------------------------------------.
+def initSimParams(cParser):
+    ParamsSectionName = 'SimulationParameters'
+    SimParams.SpaceSize   = cParser.getint(ParamsSectionName, 'spacesize')
+    SimParams.StepsN      = cParser.getint(ParamsSectionName, 'steps')
+    SimParams.Temperature = cParser.getfloat(ParamsSectionName, 'temperature')
+    SimParams.StepTime    = cParser.getfloat(ParamsSectionName, 'time_per_step')
+
+    SimParams.ConsForceFactor = cParser.getfloat(ParamsSectionName, 's_c')
+    SimParams.DissForceFactor = cParser.getfloat(ParamsSectionName, 's_c')
+    SimParams.RandForceFactor = cParser.getfloat(ParamsSectionName, 'xi')
+
+    SimParams.GravityFactor = cParser.getfloat(ParamsSectionName, 'gravity_factor')
+    SimParams.CutoffRadius  = cParser.getfloat(ParamsSectionName, 'radius_const')
+
+    cParser.remove_section(ParamsSectionName)
+
+#------------------------------------------------------------------------------.
+def initPartTypes(cParser):
+    # Section should be removed by initSimParams()
+    assert 'SimulationParameters' not in cParser.sections()
+
+    pTypes = []
+    for partTypeSect in cParser.sections():
+        partN  = cParser.getint(partTypeSect, 'numberofparticles')
+        mass   = cParser.getfloat(partTypeSect, 'mass')
+        radius = cParser.getfloat(partTypeSect, 'radius')
+        pTypes.append((partN, mass, radius))
+    return pTypes
+
+#------------------------------------------------------------------------------.
+def initUniverse(partTypes):
+    for (pI, pT) in enumerate(partTypes, start=1):
+        Universe.extend(genInitCond2(pI, pT[0], pT[1], pT[2]))
+
+#------------------------------------------------------------------------------.
+def writeHeader(OutFilePrefix, pTypes):
+    with open(OutFilePrefix, 'wb') as f:
+        wr = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+        
+        header = [1, len(Universe), SimParams.StepsN, SimParams.StepTime, SimParams.SpaceSize]
+        wr.writerow(header)
+        wr.writerow("")
+        
+        for pT in pTypes:
+            wr.writerow(pT)
+
+#------------------------------------------------------------------------------.
+def writeUniverseState(OutFilePrefix, stepnr):
+    filename = OutFilePrefix + "_step_" + str(stepnr).zfill(6)
+    with open(filename, 'wb') as f:
+        wr = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+        
+        for line in Universe:
+            wr.writerow(line[:1] + line[3:9])
+
+#------------------------------------------------------------------------------.
 
 '''
 Generates initial condition with based on the given particle. 
 The function places randomly x Particles into the given space and returns a list of Particles.
 '''
-def genInitCond2(ptype, numOfParticles, spaceSize, mass, radius):
+def genInitCond2(ptype, numOfParticles, mass, radius):
     particles=[]
     for _ in range(numOfParticles):
-        '''currentParticle = [type, mass, radius, randrange(0, spaceSize), randrange(0, spaceSize), randrange(0, spaceSize), 0, 0, 0, 0, 0, 0]'''
         particles.append([ptype, mass, radius,
-            randrange(0, spaceSize), #x
-            randrange(0, spaceSize), #y
-            randrange(0, spaceSize), #z
+            randrange(0, SimParams.SpaceSize), #x
+            randrange(0, SimParams.SpaceSize), #y
+            randrange(0, SimParams.SpaceSize), #z
             0, 0, 0, #V
             0, 0, 0]) #A
     return particles
 
+#------------------------------------------------------------------------------.
+# see below for definition of arguments
+def conservativeForce(Sc, R, Ex, Ey, Ez):
+    T = Sc * (1.0 - R)
+    return (T*Ex, T*Ey, T*Ez)
 
-    
+def dissipativeForce(Sd, R, Vx, Vy, Vz, Ex, Ey, Ez):
+    T = -1 * Sd*(1.0-R)**2 * (Vx*Ex + Vy*Ey + Vz*Ez)
+    return (T*Ex, T*Ey, T*Ez)
+
+def randomForce(Sr, Sd, kB, T, stepT, R, Ex, Ey, Ez):
+    # TODO Nie ma zmiennej losowej!!!
+    T = (2*Sd*kB*T/stepT)**0.5 * (1.0 - R) * Sr
+    return (T*Ex, T*Ey, T*Ez)
+
 #------------------------------------------------------------------------------.
-def gravity2(P, step):
-    P[Az] -= 9.82*gravityFactor
-    return P
+def applyForces(P, pToQ, pqRelDist):
+# args: P - particle
+#       pToQ - normalized vector from P to Q (other particle affecting P)
+#       pqRelDist - distance between P and Q relative to CutoffRadius
+    k_b = 1.3806488 * 10.0 **(-23)
     
-#------------------------------------------------------------------------------.
-def applyForces(P, R, step, e, Sc, Sd, xi, temperature):
-    k_b=1.3806488 * 10.0**(-23)
+    fCons = conservativeForce(SimParams.ConsForceFactor, pqRelDist,
+            pToQ[0], pToQ[1], pToQ[2])
+    print('fCons: {}'.format(fCons))
     
-    #conservativeForce(Sc, R, e_x, e_y, e_z)
-    fCons=conservativeForce(Sc, R, e[0], e[1], e[2])
+    fDiss = dissipativeForce(SimParams.DissForceFactor, pqRelDist,
+            P[Vx], P[Vy], P[Vz], pToQ[0], pToQ[1], pToQ[2])
+    print('fDiss: {}'.format(fDiss))
     
-    #dissipativeForce(Sd, R, v_x, v_y, v_z, e_x, e_y, e_z)
-    fDiss=dissipativeForce(Sd, R, P[6], P[7], P[8], e[0], e[1], e[2])
-    
-    #randomForce(xi,Sd,k_b,temperature,delta_t, R, e_x, e_y, e_z)
-    fRand=randomForce(xi,Sd,k_b,temperature,step, R, e[0], e[1], e[2])
+    fRand = randomForce(SimParams.RandForceFactor, SimParams.DissForceFactor,
+            k_b, SimParams.Temperature, SimParams.StepTime, pqRelDist,
+            pToQ[0], pToQ[1], pToQ[2])
+    print('fRand: {}'.format(fRand))
     
     massRev = 1.0 / P[Mass]
-    accelChange=(
+    accelChange = (
             massRev * (fCons[0] + fDiss[0] + fRand[0]),
             massRev * (fCons[1] + fDiss[1] + fRand[1]),
             massRev * (fCons[2] + fDiss[2] + fRand[2]))
@@ -134,209 +165,136 @@ def applyForces(P, R, step, e, Sc, Sd, xi, temperature):
     P[Ax] += accelChange[0]
     P[Ay] += accelChange[1]
     P[Az] += accelChange[2]
-    return P
         
 #------------------------------------------------------------------------------.
-def calculateNewPositions(particles, step, spacesize):
-    for p in particles:
-        currentVelocity=(p[Vx], p[Vy], p[Vz])
-        
-        #calculate current velocity + acceleration
-        p[Vx] += p[Ax] * step
-        p[Vy] += p[Ay] * step
-        p[Vz] += p[Az] * step
-        
-        #calculate new position
-        p[Rx] += ((currentVelocity[0]+p[Vx]) / 2) * step
-        p[Ry] += ((currentVelocity[1]+p[Vy]) / 2) * step
-        p[Rz] += ((currentVelocity[2]+p[Vz]) / 2) * step
-
-        # if any coordinate jumps out of space, wrap it around
-        if p[Rx] < 0 or p[Rx] > spacesize:
-            p[Rx] %= spacesize
-
-        if p[Ry] < 0 or p[Ry] > spacesize:
-            p[Ry] %= spacesize
-
-        if p[Rz] < 0 or p[Rz] > spacesize:
-            p[Rz] %= spacesize
-            
-        p[Ax]=0
-        p[Ay]=0
-        p[Az]=0
-    return particles
-
-#------------------------------------------------------------------------------.
-def relativeVec(P, Q):
-    #e vector
-    Ei=[P[3]-Q[3], P[4]-Q[4], P[5]-Q[5]]
+def relativeVec(P, Q, pqDist):
+    #vector pointing from P to Q
+    pToQ = [P[3]-Q[3], P[4]-Q[4], P[5]-Q[5]]
 
     #normalization
-    L = (Ei[0]**2.0 + Ei[1]**2.0 + Ei[2]**2.0) ** 0.5
-    print('L: {}'.format(L))
-    if L!=0:
-        Ei[0]/=L
-        Ei[1]/=L
-        Ei[2]/=L
-    return Ei
+    if pqDist!=0:
+        pToQ[0] /= pqDist
+        pToQ[1] /= pqDist
+        pToQ[2] /= pqDist
+    return pToQ
 
 #------------------------------------------------------------------------------.
-def twoParticles(P, Q, step, Rcut, s_c, s_d, xi, temperature):
+def twoParticles(P, Q):
     
-    distance=((P[Rx]-Q[Rx]) ** 2.0 +
+    pqDist=((P[Rx]-Q[Rx]) ** 2.0 +
             (P[Ry]-Q[Ry]) ** 2.0 +
             (P[Rz]-Q[Rz]) ** 2.0) ** 0.5
+    print('P to Q dist: {}'.format(pqDist))
     
-    if distance < Rcut:
-        r = distance / Rcut
+    if pqDist < SimParams.CutoffRadius:
+        pqRelDist = pqDist / SimParams.CutoffRadius
 
-        Ei = relativeVec(P, Q)
-        #Ej=(state[j][3]-state[i][3], state[j][4]-state[i][4], state[j][5]-state[i][5])
+        # TODO Ten wektor de facto wskazuje od Q do P. To daje dobre wyniki dla
+        # sił, jeśli to dyssypatywna ma minus. Zamienić nazwę zmiennej???
+        pToQ = relativeVec(P, Q, pqDist)
         
-        Ej=(Ei[0]*(-1), Ei[1]*(-1), Ei[2]*(-1))
-       
+        qToP = (pToQ[0]*(-1), pToQ[1]*(-1), pToQ[2]*(-1))
         
-        P = applyForces(P, r, step, Ei, s_c, s_d, xi, temperature)
-        Q = applyForces(Q, r, step, Ej, s_c, s_d, xi, temperature)
-    return [P, Q]
+        applyForces(P, pToQ, pqRelDist)
+        applyForces(Q, qToP, pqRelDist)
     
 #------------------------------------------------------------------------------.
-def nextStep2(state, step, spacesize):
+def applyGravity(P):
+    P[Az] -= 9.82*SimParams.GravityFactor
+    
+#------------------------------------------------------------------------------.
+def calculateNewPositions(p):
+    stepT = SimParams.StepTime
+    spcSz = SimParams.SpaceSize
+
+    currentVelocity=(p[Vx], p[Vy], p[Vz])
+    
+    #calculate current velocity + acceleration
+    p[Vx] += p[Ax] * stepT
+    p[Vy] += p[Ay] * stepT
+    p[Vz] += p[Az] * stepT
+    
+    #calculate new position
+    p[Rx] += ((currentVelocity[0]+p[Vx]) / 2) * stepT
+    p[Ry] += ((currentVelocity[1]+p[Vy]) / 2) * stepT
+    p[Rz] += ((currentVelocity[2]+p[Vz]) / 2) * stepT
+
+    # if any coordinate jumps out of space, wrap it around
+    if p[Rx] < 0 or p[Rx] > spcSz:
+        p[Rx] %= spcSz
+
+    if p[Ry] < 0 or p[Ry] > spcSz:
+        p[Ry] %= spcSz
+
+    if p[Rz] < 0 or p[Rz] > spcSz:
+        p[Rz] %= spcSz
+        
+    p[Ax]=0
+    p[Ay]=0
+    p[Az]=0
+
+#------------------------------------------------------------------------------.
+def nextStep():
     
     '''apply forces...'''
-    cnt=0
-    for i in range(0, len(state)):
-        for j in range(i+1, len(state)):
-            result = twoParticles(state[i], state[j], step, radius_const, s_c, s_d, xi, temperature)
-            state[i]=result[0]
-            state[j]=result[1]
-            cnt+=1
-                
-    #print cnt
+    for i in range(0, len(Universe)):
+        for j in range(i+1, len(Universe)):
+            # TODO może tutaj liczyć odległość i wołać jeśli < CutOffRadius???
+            twoParticles(Universe[i], Universe[j])
+        # TODO może tutaj dodawać grawitację?
            
-            #print state[i][3] #3,4,5 is position. 2 is radius      
     '''apply gravity'''
-    for p in state:
-        p=gravity2(p, step)  
+    for p in Universe:
+        applyGravity(p)  
     
     '''Calculate new positions'''
-    particles = calculateNewPositions(state, step, spacesize)
-    
-    return particles
+    for p in Universe:
+        calculateNewPositions(p)
 
 #------------------------------------------------------------------------------.
 if __name__ == "__main__":
-    print('start')
-    log = open("log", 'wb')
-    
-    wr_log = csv.writer(log, quoting=csv.QUOTE_NONNUMERIC)
-        
-    
-    
+    print('Program starting.')
     
     #Program start
     aparser = argparse.ArgumentParser()
-    aparser.add_argument('infile', help='input parameter')
+    aparser.add_argument('ConfigFile', help='input parameter')
     aparser.add_argument('outfile', help='output file')
     
     args = aparser.parse_args()
     
-    #generate from inpufile
-    Config = ConfigParser.ConfigParser()
-    Config.read(args.infile)
-    
+    # TODO Odkomentować!!!
+    #OutDirContent = os.listdir(args.OutDir)
+    #if OutDirContent:
+    #    print('Output directory is not empty! Do you want to erase it? [y/n]')
+    #    if raw_input() == 'y':
+    #        for f in OutDirContent: os.remove(f)
     #clean output folder
     for fl in glob.glob(args.outfile + "*"):
         os.remove(fl)
+
+    # Read configuration file.
+    cParser = ConfigParser.RawConfigParser()
+    cParser.read(args.ConfigFile)
+
+    # Initialize simulation parameters...
+    initSimParams(cParser)
+    # ... and list of particle types.
+    partTypes = initPartTypes(cParser)
+
+    # Initialize particles in 'Universe' list (randomize positions, zero other params)
+    initUniverse(partTypes)
+
+    # Save header file - mostly simulation parameters.
+    writeHeader(args.outfile, partTypes)
     
-    #f = open(args.infile)
-    #lines = f.readlines()
-    #f.close()
+    # Save initial state
+    writeUniverseState(args.outfile, 0)
     
-    spacesize = int(ConfigSectionMap("SimulationParameters") ['spacesize'])
-    simulatedSteps = int(ConfigSectionMap("SimulationParameters") ['steps'])
-    s_c = float(ConfigSectionMap("SimulationParameters") ['s_c'])
-    s_d = float(ConfigSectionMap("SimulationParameters") ['s_d'])
-    xi = float(ConfigSectionMap("SimulationParameters") ['xi'])
-    temperature = float(ConfigSectionMap("SimulationParameters") ['temperature'])
-    timePerStep = float(ConfigSectionMap("SimulationParameters") ['time_per_step'])
-    gravityFactor = float(ConfigSectionMap("SimulationParameters") ['gravity_factor'])
-    radius_const = float(ConfigSectionMap("SimulationParameters") ['radius_const'])
-    #gravityFactor = 0.001
+    # Actual simulation loop.
+    for i in range(1, SimParams.StepsN):
+        nextStep()
+        writeUniverseState(args.outfile, i)
+        print ("%(i)i/%(total)i" % {"i":i+1, "total":SimParams.StepsN})
     
-    
-    #timePerStep=0.1
-    
-    
-    numberOfTypes = len(Config.sections()) - 1
-    
-    numberOfParticles=[]
-    mass=[]
-    radius=[]
-    
-    for p in range (1, len(Config.sections())):
-        numberOfParticles.append(int(ConfigSectionMap(Config.sections()[p]) ['numberofparticles']))
-        mass.append(int(ConfigSectionMap(Config.sections()[p]) ['mass']))
-        radius.append(int(ConfigSectionMap(Config.sections()[p]) ['radius']))
-    
-    
-    totalNumberOfParticles=0
-    for p in numberOfParticles:
-        totalNumberOfParticles+=p
-    
-    #timePerStep=0.005
-    numberOfStates=simulatedSteps*totalNumberOfParticles
-    
-    state=[]
-    for i in range(0, numberOfTypes):
-        state.extend(genInitCond2(ptype=i+1, numOfParticles=numberOfParticles[i], spaceSize=spacesize, mass=mass[i], radius=radius[i]))
-    
-    
-    #Deprecated: Used for classic one file output
-    #world_history = []
-    #world_history.extend(copy.deepcopy(state))
-    
-    write_Header(simulatedSteps, totalNumberOfParticles, timePerStep, spacesize)
-    
-    #save initial state
-    save_state(state, 0)
-    
-    for i in range(1, simulatedSteps):
-        state=nextStep2(state, timePerStep, spacesize)
-        save_state(state, i)
-        print ("%(i)i/%(total)i" % {"i":i+1, "total":simulatedSteps})
-        #Deprecated: Used for classic one file output
-        #world_history.extend(copy.deepcopy(state))
-    
-    #Deprecated: Used for classic one file output    
-    #save also in one plike for compatibility purposes
-    #f = open("output_classic", 'wb')
-        
-    #header = [1,totalNumberOfParticles, simulatedSteps, timePerStep, spacesize]
-    
-    #wr = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-    
-    #wr.writerow(header)
-    #wr.writerow("")
-    
-    #i=1
-    #for (n, m) in zip(numberOfParticles,mass):
-    #    wr.writerow([i, n, m])
-    #    i+=1
-    
-    
-    #wr.writerow("")
-    #cnt = 0
-    #for line in world_history:
-    #    if cnt == totalNumberOfParticles:
-    #        wr.writerow("")
-    #        cnt=0
-            
-    #    cnt=cnt+1
-    #    wr.writerow(line[:1] + line[3:9])
-        
-    #f.close()
-    
-        
-    
-    print("end")
+    print("Finished.")
+
--- debug.py
+++ debug.py
@@ -1,20 +1,58 @@
-#!/usr/bin/python2
+#!/usr/bin/env python2
+# -*- coding: utf8 -*-
+
+
+
+# Skrypt wypisujący wartości sił działających na 2 cząstki.
+# Potrzebuje pewnych funkcji 'print' w kodzie DPD_main!!!
+
+
 
 from DPD_main import *
 
+# Parametry imitujące prawdziwą symulację.
+SimParams.SpaceSize       = 3
+SimParams.StepsN          = 10
+SimParams.Temperature     = 100
+SimParams.StepTime        = 0.01
+
+SimParams.ConsForceFactor = 10
+SimParams.DissForceFactor = 1
+SimParams.RandForceFactor = 1
+
+SimParams.GravityFactor   = 0.0001
+SimParams.CutoffRadius    = 5
+
+# Funkcja wypisująca cząsteczkę, w ludzkim formacie.
 def printPart(P):
     print('T: {}, M: {}, R: {}\tRx: {}, Ry: {}, Rz: {}\t\
             Vx: {}, Vy: {}, Vz: {}\tAx: {}, Ay: {}, Az: {}'.format(
             P[0], P[1], P[2], P[3], P[4], P[5], P[6],
             P[7], P[8], P[9], P[10], P[11]))
 
-#def genInitCond2(ptype, numOfParticles, spaceSize, mass, radius):
-uni = genInitCond2(1, 2, 10, 1, 1)
+# Wylosowanie dwóch cząsteczek (tylko pozycji, prędkość i przyspieszenie są zerami)
+P, Q = genInitCond2(1, 2, 1, 1)
 
-printPart(uni[0])
-printPart(uni[1])
+# Początkowe wypisanie.
+printPart(P)
+printPart(Q)
 
-E = relativeVec(uni[0], uni[1])
+# Pętla imitująca działanie symulacji, oblicza interakcje między dwiema
+# powyższymi cząstkami i wypisuje ich stan.
+for i in range(100):
 
-print(E)
+    # Ten if jest przydatny: chcemy zobaczyć jak siły zachowują się po wielu
+    # krokach symulacji, szczególnie cieakwa jest siła dyssypatywna, która zależy od prędkości
+    if i>96:
+        print('Iteration----------------------------------------')
+        twoParticles(P,Q)
+
+        printPart(P)
+        printPart(Q)
+
+        calculateNewPositions(P)
+        calculateNewPositions(Q)
+
+        printPart(P)
+        printPart(Q)
 
